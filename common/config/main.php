15296ccaff5540475374d0ee3632591fd7b02867
Виталий <vgolovin@lightsoft.ru>
2018-05-06 23:35:20 +0300

    добавление rbac

--- common/config/main.php
+++ common/config/main.php
@@ -22,15 +22,24 @@ return [
         ],
         'i18n' => [
             'translations' => [
-                '*' => [
+                'yii2mod.rbac' => [
                     'class' => 'yii\i18n\PhpMessageSource',
-                    'basePath' => '@app/messages', // if advanced application, set @frontend/messages
-                    'sourceLanguage' => 'en',
-                    'fileMap' => [
-                        //'main' => 'main.php',
-                    ],
+                    'basePath' => '@yii2mod/rbac/messages',
                 ],
+                // ...
             ],
         ],
     ],
+    /*'as access' => [
+        'class' => yii2mod\rbac\filters\AccessControl::className(),
+        'allowActions' => [
+            'site/*',
+            'admin/*',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
+    ],*/
 ];
